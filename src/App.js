70017e7d9da0751cfd2369eb8c4a87e939094445
Alex C <l3xscrubs@gmail.com>
2019-05-28 10:34:53 -0400

    adjust text for first commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to make magic happen.
+          Edit <code>src/App.js</code> and save to make magic happen and an initial commit.
         </p>
         <a
           className="App-link"
